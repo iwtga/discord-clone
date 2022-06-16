33519eb7c8c20bfd83dc5029962c4101ec4823ac
Alan Almeida <alan241015@gmail.com>
2022-06-16 13:00:07 +0530

    add dynamic routing and dummy db

--- base/views.py
+++ base/views.py
@@ -11,5 +11,9 @@ def home(request):
     context = {"rooms": rooms}
     return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'base/room.html')
+def room(request, pk):
+    context = {"room": None}
+    for i in rooms:
+        if i['id'] == int(pk):
+            context["room"] = i
+    return render(request, 'base/room.html', context)
