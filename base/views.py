29131d2a24e6182bfadade871b929dbaa4786511
Alan Almeida <alan241015@gmail.com>
2022-06-16 12:45:46 +0530

    add templates for base + add inheritance

--- base/views.py
+++ base/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+rooms = [
+    {'id': 1, 'name': 'Lets learn Python'},
+    {'id': 2, 'name': 'Lets learn JavaScript'},
+    {'id': 3, 'name': 'Lets learn Shell Scripting'},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    context = {"rooms": rooms}
+    return render(request, 'base/home.html', context)
 
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'base/room.html')
