f1b6f318470cddfe1d7063ddd56ad7f318a8e341
Alan Almeida <alan241015@gmail.com>
2022-06-16 13:47:21 +0530

    add admin superuser, add room model, edit views

--- base/models.py
+++ base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    # host =
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # topic =
+    # participants =
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
--- base/views.py
+++ base/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
-rooms = [
-    {'id': 1, 'name': 'Lets learn Python'},
-    {'id': 2, 'name': 'Lets learn JavaScript'},
-    {'id': 3, 'name': 'Lets learn Shell Scripting'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn Python'},
+#     {'id': 2, 'name': 'Lets learn JavaScript'},
+#     {'id': 3, 'name': 'Lets learn Shell Scripting'},
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {"rooms": rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
     context = {"room": None}
-    for i in rooms:
-        if i['id'] == int(pk):
-            context["room"] = i
+    context["room"] = Room.objects.get(id=pk)
     return render(request, 'base/room.html', context)
